8fdde203210d63a2b786f328bc55ae6de935f1dc
David <davidpoyner12@Davids-MacBook-Pro.local>
2016-08-16 13:07:13 -0400

    updating for node

---
+++ README.md
@@ -0,0 +1,92 @@
+# dhaak-anim
+=============
+
+A simple tween class independant from third party libraries.
+
+# Getting Started
+
+## Installation
+
+```
+npm install dhaak-anim 
+git clone https://github.com/dhaakon/dhaak-anim
+```
+
+## Examples
+```
+npm install
+grunt examples
+```
+
+Check out the source code in `/examples/src/app.js`
+
+
+## Usage
+Dhaak Tween is based off of dojo's tweening syntax. The user provides
+an easing function, a target node, and a callback and the tween engine
+does the rest. For example, to tween a circle 200 pixels to the right we
+would do the following:
+
+```
+var circle = document.getElementById('circle');
+
+// create the tween object
+var tween = new Tween();
+
+var start = 0;
+var end = 200;
+
+// create the options object
+var options = {
+	duration: 1000, // 1 second
+	node: circle, // the object to act on
+	curve: [start, end], // the curve object
+	onUpdate: function(c){
+		this.node.style.left = c + "px";
+	}
+};
+
+// play the tween with the specified options
+tween.play(options)
+
+```
+
+### Curve/Line objects
+Dhaak Tween relies on either a curve or line object in order to advance the animation. The curve object is an array containing start and finish values, like so:
+
+`curve = [ 0, 100 ]`
+
+This onUpdate function will pass through a variable indicating the change value at the correct tween time.
+
+```
+var onUpdate = function(c){
+	this.node.style.left = c + "px";
+}
+```
+
+What if we wanted to pass in multiple values? That would be nice.
+
+We can with a Line object. A line object is an array containing an array of start and end values. The onUpdate function will then pass over an array with the curve values at the specific time.
+
+Let's say we wanted to move the object 200 pixels left and rotate 30 degrees. We would first create the Line object:
+
+```
+var startValues = [ 0, 0 ];
+var endValues = [ 200, 30 ];
+var line = new Tween.Line( startValues, endValues );
+```
+
+Then the onUpdate function will pass over an array with the position (0 index) and the rotation (1 index) values:
+
+```
+var onUpdate = function(c){
+	var positionValue = c[0];
+	var rotationValue = c[1];
+	
+	this.node.style.left = positionValue + "px";
+	this.node.style.transform = "rotate(" + rotationValue + "deg)";
+}
+```
+
+This method allows for an unlimited amount of property adjustments per tween.
+
---
+++ examples/src/timeline.js
@@ -0,0 +1,87 @@
+var Timeline = require('../../dhaak-timeline.js');
+var Tween = require('../../dhaak-tween.js');
+var Easing = require('penner');
+var _ = require('underscore');
+var d3 = require('d3');
+
+(function init(){
+  var cos = Math.cos, sin = Math.sin;
+
+  console.log("Timeline example");
+
+  var createCircle = function(){
+    var _circle = document.createElement("div");
+    _circle.style.width = "20px";
+    _circle.style.left = "400px";
+    _circle.style.top = "200px";
+    _circle.style.height = "20px";
+    _circle.style.display = "block";
+    _circle.style.background = "red";
+    _circle.style.borderRadius = "10px";
+    _circle.style.position = "absolute";
+    _circle.style['-webkit-transform'] = "scale(0)";
+
+    return _circle;
+  }
+
+  var circle = createCircle();
+
+  document.body.appendChild(circle);
+
+  var tween1 = new Tween( { duration: 1000, delay: 100 } );
+
+  tween1.curve(new Tween.Line( [   0,    0,  50,    0,  300 ],
+                               [ 360,  360,  50,  1.5,  300 ]))
+        .ease(Easing.easeInOutQuad)
+        .begin(function(){
+          console.log('1 start');
+        })
+        .update(function(c){
+            var _r = c[2] * c[3];
+            var _x = cos(c[0] * ((Math.PI * 2) / 180)) * _r;
+            var _y = sin(c[1] * ((Math.PI * 2) / 180)) * _r;
+            var _offset = c[4];
+
+            var rx = 0, ry = 0, rz = 70;
+
+            mat = new WebKitCSSMatrix('scale(' + c[3] + ')').rotate( rx, ry, rz ).translate( _x, _y, 0).toString();
+
+            circle.style['-webkit-transform'] = mat
+        })
+        .end(function(){
+          console.log('1 end');
+        });
+
+  var tween2options = {
+    duration: 400,
+    delay:200,
+    curve:new Tween.Line([1,100],[ 10, 255 ]),
+    easing:Easing.easeInQuad,
+    onUpdate:function(c){
+      var b = Math.round(c[1]);
+      circle.style['-webkit-transform'] = new WebKitCSSMatrix(circle.style['-webkit-transform']).rotate(180,0,1).toString();
+      circle.style.background = 'rgb(' + b + ',0,' + b + ')';
+      circle.style.border = c[0] + 'px red';
+    },
+    onEnd:function(){
+    }
+  }
+
+  var tween2 = new Tween( tween2options );
+
+  var options = {
+    duration: 1000
+  };
+
+  var tween1clone = tween1.clone();
+  tween1clone.reverse();
+
+  var timeline = new Timeline( options );
+  timeline.addTweens([ tween1, tween1]);
+  timeline.loop( true );
+
+  setTimeout(function(){
+    timeline.start();
+  }, 200);
+
+})()
